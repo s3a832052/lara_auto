e9c4c1d828057433a1103e48c5ad0abb5707c857
GuoTingRui <azsxdc890902@gmail.com>
2021-11-06 23:09:12 +0800

    加入monolog測試程式碼

--- public/index.php
+++ public/index.php
@@ -11,3 +11,14 @@ $vincent= new Hello\Someone('Vincent');
 // 以下Someone類別的使用可以不用use Demo\Hello;
 $mary= new \Demo\Hello\Someone('Mary');
 $john= new Demo\Hello\Someone('John');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
